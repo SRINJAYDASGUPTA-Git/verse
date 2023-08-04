32df5fd0af492dcd7aafcac30798316aaa6244e7
SRINJAYDASGUPTA-Git <dasguptasrinjay2004@gmail.com>
2023-08-04 23:35:49 +0530

    Made Layout

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,27 @@
+import { ClerkProvider } from "@clerk/nextjs"
+import {Inter} from "next/font/google";
+
+import '../globals.css'
+
+export const metadata={
+    title:'Verse',
+    description:'Discover a vibrant social media experience with Verse - the innovative platform that combines the best of Threads with an array of additional features. Join diverse communities, engage in meaningful conversations, and connect with like-minded individuals from around the world. Embrace a new dimension of social networking with Verse, where limitless possibilities await.'
+}
+
+const inter = Inter({subsets: ["latin"]})
+
+export default function RootLayout({
+    children
+}:{
+    children:React.ReactNode
+}){
+    return (
+        <ClerkProvider>
+            <html lang='en'>
+                <body className={`${inter.className}bg-dark-1`} >
+                    {children}
+                </body>
+            </html>
+        </ClerkProvider>
+    )
+}
